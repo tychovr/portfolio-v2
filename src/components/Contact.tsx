e164ff29f7d8c5e80516c1c230d58d2263e44263
tychovr <tychovanrosmalen12@gmail.com>
2025-10-08 11:20:09 +0000

    Set new fetch URL

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -36,7 +36,7 @@ export default function Contact() {
     try {
       console.info("Sending email with data:", data);
 
-      const response = await fetch(`http://localhost:8787/send-contact`, {
+      const response = await fetch(`/api/send-contact`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
